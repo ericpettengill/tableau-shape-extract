e1fe684a5f2973c31358db21caa27acfc67c31b8
Eric Pettengill <ericpettengill@gmail.com>
2022-12-21 13:59:50 -0600

    update extract.py and README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # tableau-shape-extract
+
+## Setup
+
+```shell
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install -r requirements.txt
+```
+
+## Run
+
+```python
+# extract.py
+from extract import main
+
+main('path/to/workbook.twb')
+```
--- extract.py
+++ extract.py
@@ -1,25 +1,40 @@
 from bs4 import BeautifulSoup
 import base64
 import os
+import pathlib
 
-with open(r"path/to/workbook.twb", 'r') as f:
-    y = BeautifulSoup(f)
 
-out = {}
-for shapes in y.find_all("shapes"):
-    for img in shapes.find_all('shape'):
-        out.update({
-            img.attrs['name']: img.text
-        })
+def main(workbook_path: str):
+    """
 
-for name, content in out.items():
-    folder, file = name.split('/')
-    print(f"{file}: {os.path.isfile(name)}")
+    :param workbook_path: string
+        /path/to/workbook.twb
+    :return: None
+    """
+    fp = pathlib.Path(workbook_path)
+    assert fp.suffix == '.twb', f"workbook must be *.twb file, you passed {fp.suffix}"
+    with open(fp, 'r') as f:
+        y = BeautifulSoup(f)
 
-    if not os.path.isdir(folder):
-        print(f"making directory {folder}")
-        os.mkdir(f"{folder}")
+    out = {}
+    for shapes in y.find_all("shapes"):
+        for img in shapes.find_all('shape'):
+            out.update({
+                img.attrs['name']: img.text
+            })
 
-    print(f"writing to file: {name}")
-    with open(f"{name.split('.')[0]}.png", 'wb') as f:
-        f.write(base64.b64decode(content))
+    for name, content in out.items():
+        folder, file = name.split('/')
+        print(f"{file}: {os.path.isfile(name)}")
+
+        if not os.path.isdir(folder):
+            print(f"making directory {folder}")
+            os.mkdir(f"{folder}")
+
+        print(f"writing to file: {name}")
+        with open(f"{name.split('.')[0]}.png", 'wb') as f:
+            f.write(base64.b64decode(content))
+
+
+if __name__ == '__main__':
+    main('path/to/workbook.twb')
